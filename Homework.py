43c28f3e0a8fae6b303c25bd7e098357ca2a4aeb
OlgaLesik <o6467518@gmail.com>
2024-09-13 21:31:15 +0300

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,25 @@
+# 1st program
+
+print(int((9 ** 0.5) * 5))
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print((2 * 2) + 2)
+print(2 * (2 + 2))
+print(((2 * 2) + 2) < (2 * (2 + 2)))
+print(((2 * 2) + 2) > (2 * (2 + 2)))
+print(((2 * 2) + 2) == (2 * (2 + 2)))
+
+# 4th program
+print(float('123.456'))
+print(123.456 * 10)
+print(int(1234.56 % 10))
+print(int(1234.56 - 4))
+print(int(1234.56 + 4))
+print(int(1234.56 * 4))
+print(1234.56 < 4)
+print(1234.56 > 4)
+print(1234.56 < 4 and 1234 > 4)
+print(1234.56 > 4 or 1234 > 4)
