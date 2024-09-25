c814dc5b2864e9159c9af8845f523486d877ccbd
OlgaLesik <o6467518@gmail.com>
2024-09-25 11:08:32 +0300

    Add files via upload

---
+++ Homework_Variables.py
@@ -0,0 +1,19 @@
+Number_of_completed_remote_sensing = 12
+print(Number_of_completed_remote_sensing)
+Number_of_hours_spent = 1.5
+print(Number_of_hours_spent)
+Course_name = 'Python'
+print(Course_name)
+Number_of_hours_spent = (int(Number_of_completed_remote_sensing // Number_of_hours_spent))
+print(Number_of_hours_spent)
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+# print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+# Vasya дал Petya 10 яблок и 15 апельсинов
+course = "Курс"
+course_name = "Python"
+total_tasks = 12
+hours_spent = 1.5
+average_execution_time = 0.125
+print('Курс: ',course_name,', всего задач: ', total_tasks, ',', ' затрачено часов: ', hours_spent, ',',
+' среднее время выполнения ', average_execution_time, ' часа.', sep='')
+
