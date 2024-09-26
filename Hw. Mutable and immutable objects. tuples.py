7ac813cf5043092a18934fc4451e83783670fe86
OlgaLesik <o6467518@gmail.com>
2024-09-26 10:21:59 +0300

    Add files via upload

---
+++ Hw. Mutable and immutable objects. tuples.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 3, 4, True, "String"
+print(immutable_var)
+# immutable_var[0] = 'peach'
+# you can't change a tuple
+mutable_list = ['apple', 'banana', 'coconut']
+print(mutable_list)
+mutable_list[0] = 'peach'
+print(mutable_list)
