9af28b76b267ec8e670d67fd1cd935c5093efff8
OlgaLesik <o6467518@gmail.com>
2024-09-26 16:07:12 +0300

    Update Homework_Variables.py

--- Homework_Variables.py
+++ Homework_Variables.py
@@ -1,6 +1,3 @@
-Number_of_completed_remote_sensing = 12
-print(Number_of_completed_remote_sensing)
-Number_of_hours_spent = 1.5
 print(Number_of_hours_spent)
 Course_name = 'Python'
 print(Course_name)
@@ -9,11 +6,13 @@ print(Number_of_hours_spent)
 # Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
 # print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
 # Vasya дал Petya 10 яблок и 15 апельсинов
-course = "Курс"
 course_name = "Python"
-total_tasks = 12
 hours_spent = 1.5
-average_execution_time = 0.125
+total_tasks = 12
+float(total_tasks)
+print(float(total_tasks))
+average_execution_time = hours_spent / (float(total_tasks))
+print(average_execution_time)
 print('Курс: ',course_name,', всего задач: ', total_tasks, ',', ' затрачено часов: ', hours_spent, ',',
 ' среднее время выполнения ', average_execution_time, ' часа.', sep='')
 
